7f04927874f91d38c64973ebffb05e1221cc90c6
Jalen <jamcbride131@gmail.com>
2025-03-26 18:40:18 -0500

    Added commas to the end of my arrays and added the output for all 3 arrays

--- Practice2dArrays.js
+++ Practice2dArrays.js
@@ -1,22 +1,28 @@
 //Task 1
 
 let allScores = [
-    [89,86,90]
-    [90,90,85]
+    [89,86,90],
+    [90,90,85],
     [76,80,84]
 ]
 
 //Task 2
 
 let theatreSeating = [
-    ["A1","A2","A3","A4"]
-    ["B1","B2","B3","B4"]
-    ["C1","C2","C3","C4"]
+    ["A1","A2","A3","A4"],
+    ["B1","B2","B3","B4"],
+    ["C1","C2","C3","C4"],
 ]
 
 //Task 3
 
 let contactsList = [
-    ["Harry Osborn","HarryOz@gmail.com","123-456-7890"]
+    ["Harry Osborn","HarryOz@gmail.com","123-456-7890"],
     ["Peter Parker", "spiderman@gmail.com", "555-555-5555"]
 ]
+
+console.log(allScores);
+
+console.log(theatreSeating);
+
+console.log(contactsList);
